9527f77a5153e30e97c92f1b4d389df2fe62bf51
Stan402 <konvin@yahoo.com>
2017-07-19 16:19:41 +0300

    database added, basic methods added

--- Java3/src/ru/geekbrains/java3/lesson2/Homework.java
+++ Java3/src/ru/geekbrains/java3/lesson2/Homework.java
@@ -1,5 +1,53 @@
 package ru.geekbrains.java3.lesson2;
 
+import java.sql.*;
+
 public class Homework {
 
+    private static Connection connection;
+    private static Statement statement;
+    private static PreparedStatement prStmt;
+
+
+    public static void main(String[] args) {
+
+        connect();
+
+        disconnect();
+
+    }
+
+    public static void initDB(){
+        connect();
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        disconnect();
+    }
+
+    public static void connect(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:Lesson2hw.db");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void disconnect(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+//    CREATE TABLE Goods (
+//        id     INTEGER PRIMARY KEY AUTOINCREMENT,
+//        prodid STRING,
+//        title  TEXT,
+//        cost   INTEGER
+//);
