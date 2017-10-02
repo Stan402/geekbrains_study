0ca0ae9a79aa5dc6b2b9beab95718f1dede420fe
Stan402 <konvin@yahoo.com>
2017-10-02 17:45:38 +0300

    Network tasks saved

---
+++ Network1/Homework8/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTLM>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Test1</title>
+</head>
+
+<body>
+<?php
+print 'Hello World! This is Stan calling :)<br>';
+phpinfo();
+?>
+</body>
+</html>
