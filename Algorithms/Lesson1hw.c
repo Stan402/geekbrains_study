61a281ba6b51ff618540adf884ea6b2f46c6e4f6
Stan402 <konvin@yahoo.com>
2017-10-03 15:46:57 +0300

    Algorithms: Homework1 is done.

--- Algorithms/Lesson1hw.c
+++ Algorithms/Lesson1hw.c
@@ -3,10 +3,25 @@
  *
  *  Created on: Oct 2, 2017
  *      Author: stan
+ *      Константин Виноградов
+ *      Условия задач:
+ *
+ *      12. Написать функцию нахождения максимального из трех чисел.
+ *
+ *      13. * Написать функцию, генерирующую случайное число от 1 до 100.
+ *      а) с использованием стандартной функции rand()
+ *      б) без использования стандартной функции rand()
+ *
+ *      14. *Автоморфные числа. Натуральное число называется автоморфным,
+ *      если оно равно последним цифрам своего квадрата. Например, 252 = 625.
+ *       Напишите программу, которая вводит натуральное число N и выводит на экран
+ *        все автоморфные числа, не превосходящие N.
+ *
  */
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 
 void printAutomorphicNumbers(int num){
@@ -29,12 +44,29 @@ void printAutomorphicNumbers(int num){
 	printf("\n\n");
 }
 
+int getRnd(){
+	return rand() % 100 + 1;
+}
+
+int getMyRnd(int seed){
+	int a,b,x,mod;
+	mod = 100;
+	a = 16645;
+	b = 101390;
+	return (a * seed + b) % 100 + 1;
+}
+
+int findMax(int a, int b, int c){
+	int max = b > a ? b : a;
+	return c > max ? c : max;
+}
+
 
 int main(){
 	int next;
 	do{
 		printf("Enter task number or 0 for exit: \n");
-		printf("only tasks 14, 13 realized so far...\n");
+		printf("only tasks 14, 13 and 12 realized so far...\n");
 		scanf("%d", &next);
 		switch (next){
 		case 14:
@@ -44,6 +76,38 @@ int main(){
 			printAutomorphicNumbers(num);
 			break;
 		case 13:
+			printf("\nThis function returns random numbers from 1 to 100\n");
+			int am;
+			int flag;
+			printf("\nEnter amount of random numbers you need:");
+			scanf("%d", &am);
+			if(am <= 0) break;
+			printf("\nStandart randomizer or custom? 1/0 \n");
+			scanf("%d", &flag);
+			printf("\n");
+			if (flag){
+			for(int i = 0; i < am; i++){
+				printf("%d ", getRnd());
+			}
+			} else {
+				int seed;
+				printf("Enter seed:\n");
+				scanf("%d", &seed);
+				for(int i = 0; i < am; i++){
+					seed = getMyRnd(seed);
+					printf("%d ", seed);
+				}
+			}
+			printf("\n");
+			break;
+		case 12:
+			printf("\nThis function returns random numbers from 1 to 100\n");
+			int a, b, c;
+			printf("Enter 3 number:\n");
+			scanf("%d", &a);
+			scanf("%d", &b);
+			scanf("%d", &c);
+			printf("Maximum of 3 is: %d\n\n", findMax(a, b, c));
 			break;
 		case 0:
 			printf("\nThank you! See you next time...");
